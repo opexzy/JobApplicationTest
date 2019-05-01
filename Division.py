e89c9abea821f7fbd11edf18df115928cd315584
Akosile Opeyemi <opeyemiakosile@gmail.com>
2019-05-01 15:58:12 +0100

    Akosile Opeyemi Samuel

---
+++ Division.py
@@ -0,0 +1,6 @@
+from DivisionOperation import Division
+
+num = input("Enter The Numerator: ")
+den = input("Enter The Denominator: ")
+
+Division(int(num),int(den)).calc()
--- DivisionOperation.py
+++ DivisionOperation.py
@@ -50,6 +50,9 @@ class Division:
             exit()
         if(self.nume == 0):
             print("Quotient = " + 0 + ", Remainder = " + 0)
+        if(self.nume < 0 and self.deno < 0):
+            self.nume = abs(self.nume)
+            self.deno = abs(self.deno)
         self.generateList()
         self.binarySearchHelper()
         self.last_mid = self.mid_value
@@ -82,12 +85,37 @@ class Division:
                 else:
                     self.last_mid = self.mid_value
                     break
-        elif(self.nume > 0 and self.deno < 0):
+            remainder = self.nume - self.deno
+        elif(self.nume < 0 and self.deno < 0):
+            while True:
+                if (self.mid_value * self.deno) < self.nume:
+                    if (self.mid_value >= self.last_mid):
+                        self.last_mid = self.mid_value
+                    if len(self.R_div) <= 1:
+                        if (self.R_div[0] * self.deno <= self.nume):
+                            self.last_mid = self.R_div[0]
+                            break
+                        else:
+                            self.last_mid = self.mid_value
+                            break
+                    else:
+                        self.nextMidValue(self.R_div)
+                        continue
+                elif (self.mid_value * self.deno > self.nume):
+                    if len(self.L_div) <= 1:
+                        if (self.L_div[0] * self.deno <= self.nume):
+                            self.last_mid = self.L_div[0]
+                            break
+                        else:
+                            self.last_mid = self.mid_value
+                            break
+                    else:
+                        self.nextMidValue(self.L_div)
+                        continue
+                else:
+                    self.last_mid = self.mid_value
+                    break
+            remainder = self.deno - self.nume
+        else:
             return
-
-
-
-
-x = Division(6,-5)
-x.calc()
-print(x.last_mid)
+        print("Quotient: " + str(self.last_mid) + " | Remainder: " ,remainder)
